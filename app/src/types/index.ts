fabfb051f250060970c487a8dbaf8585c2cb2fdd
Bibek2442B <gnawalibibek92@gmail.com>
2026-02-07 03:15:05 +0000

    My Interface Types

---
+++ app/src/types/index.ts
@@ -0,0 +1,37 @@
+import {Timestamp} from "@firebase/firestore";
+
+export interface MyUserType {
+  id: number;
+  name: string;
+  email: string;
+  role: "ADMIN" | "OPERATOR" | "USER";
+  emailVerified: boolean;
+  approvedByAdmin: boolean;
+  createdAt: Timestamp;
+}
+
+export interface MyScheduleType {
+  startTime: Timestamp,
+  bursts: number,
+  intervalMinutes: number,
+}
+
+export interface MyRuntimeType {
+  startTime: Timestamp,
+  endTime: Timestamp,
+}
+
+export interface MyErrorType {
+  hasError: boolean,
+  errorMessage?: string,
+}
+
+export interface MySamplerType {
+  name: string,
+  address: string,
+  status: string,
+  phone: string,
+  schedule?: MyScheduleType,
+  runtime?: MyRuntimeType,
+  error?: MyErrorType,
+}
